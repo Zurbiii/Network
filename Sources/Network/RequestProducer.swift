0612a6979fe036b0d2cd591e6f6cbd4c11ebf644
Guillaume ZURBACH <guillaumezurbach@macbook-pro.home>
2023-03-10 16:43:13 +0100

    Added: Alamofire

--- Sources/Network/RequestProducer.swift
+++ Sources/Network/RequestProducer.swift
@@ -23,7 +23,10 @@ public final class RequestProducer: RequestProducerProtocol {
         dataRequest
             .validate(statusCode: 200..<300)
             .responseDecodable(completionHandler: { [weak dataRequest] (response: DataResponse<T, AFError>) in
-                dataRequest.map { debugPrint($0) }
+//                dataRequest.map { debugPrint($0) }
+//                let responseData = response.data ?? Data()
+//                let string = String(data: responseData, encoding: .utf8)
+//                print("Reponse string: \(string ?? "")")
                 switch response.result {
                 case .success(let response):
                     completion(.success(response))
