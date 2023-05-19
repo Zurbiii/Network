9337f5df011d5a771dba434c5cf8e58d3e40375a
Zurbiii <zurbiii@gmail.com>
2023-05-19 12:23:20 +0200

    Misc

--- Sources/Network/RequestProducer.swift
+++ Sources/Network/RequestProducer.swift
@@ -13,17 +13,20 @@ public final class RequestProducer: RequestProducerProtocol {
     @available(iOS 13.0, *)
     public static func request<T: Decodable>(_ apiRoute: APIRouteable) async throws -> Result<T, Error> {
         let dataRequest = AF.request(apiRoute)
-        return try withCheckedThrowingContinuation { continuation in
-            dataRequest
-                .responseDecodable(completionHandler: { [weak dataRequest] (response: DataResponse<T, AFError>) in
-                    dataRequest.map { debugPrint($0) }
-                    switch response.result {
-                    case .success(let response):
-                        continuation.resume(returning: .success(response))
-                    case .failure(let error):
-                        continuation.resume(throwing: error)
-                    }
-                })
+        Task {
+            return try withCheckedThrowingContinuation { continuation in
+                dataRequest
+                    .validate(statusCode: 200..<300)
+                    .responseDecodable(completionHandler: { [weak dataRequest] (response: DataResponse<T, AFError>) in
+                        dataRequest.map { debugPrint($0) }
+                        switch response.result {
+                        case .success(let response):
+                            continuation.resume(returning: .success(response))
+                        case .failure(let error):
+                            continuation.resume(throwing: error)
+                        }
+                    })
+            }
         }
     }
     
