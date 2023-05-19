526cd54c7ac18ed426e640da10ac351643133bcb
Zurbiii <zurbiii@gmail.com>
2023-05-19 14:13:01 +0200

    Init

--- Sources/Network/APIRoute.swift
+++ Sources/Network/APIRoute.swift
@@ -9,6 +9,18 @@ import Foundation
 import Alamofire
 
 public struct APIRoute: APIRouteable {
+    
+    public init(baseURL: String,
+                path: String,
+                method: HTTPMethod,
+                parameters: Parameters?,
+                encoding: ParameterEncoding) {
+        self.baseURL = baseURL
+        self.path = path
+        self.method = method
+        self.parameters = parameters
+        self.encoding = encoding
+    }
 
     public var baseURL: String
     public var path: String
